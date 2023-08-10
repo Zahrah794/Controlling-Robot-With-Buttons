26adf1c352e11c5aaccc6677faaa800d9c944d79
Zahrah794 <139267881+Zahrah794@users.noreply.github.com>
2023-08-10 18:58:16 +0300

    Create Retrieve.php

---
+++ Retrieve.php
@@ -0,0 +1,28 @@
+<?php
+// Connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "robotctrl";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve the last entered movement from the database
+$sql = "SELECT Direction FROM btns ORDER BY id DESC LIMIT 1";
+$result = $conn->query($sql);
+
+// Process and display the last entered movement
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $movement = $row['Direction'];
+    echo "<p>Last Entered Movement: " . $movement . "</p>";
+} else {
+    echo "No movements found.";
+}
+
+$conn->close();
+?>
+
